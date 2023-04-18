a73e4c5db13ba0679c99515487d37968e970b17f
MarianHrab <marian.l.hrab@ukd.edu.ua>
2023-04-18 23:57:00 +0300

    Changes

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -3,4 +3,44 @@ class FilmsController < ApplicationController
         #@films = Film.all
         @films = Film.all.paginate(page: params[:page], per_page: 5)
     end
+
+    def show
+        begin
+        @film = Film.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+        redirect_to films_path, alert: "Film not found"
+        end
+     end
+  
+
+    def destroy
+        @film = Film.find(params[:id])
+        if @film
+          @film.delete
+          redirect_to film_path, alert: "Film deleted successfully"
+        else
+          redirect_to film_path, alert: "Film not found"
+        end
+      end
+
+    def new
+        @film = Film.new
+    end
+
+    def create
+        def create
+            @film = Film.new(permited_attributes)
+          
+            if @film.save
+              redirect_to films_path, notice: "Film was successfully created."
+            else
+              render :new
+            end
+          end
+    end
+
+    private
+    def permited_attributes
+        params.require("film").permit("name", "year")
+    end
 end
